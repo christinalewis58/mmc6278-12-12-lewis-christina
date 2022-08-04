64e6a1e12e93d27bd20744a0f5b7a36f0c9aae78
Christina Lewis <clewis13@ufl.edu>
2022-08-04 09:09:46 -0400

    finished coding functions

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ class Word {
 
   // implement the guessLetter function:
   guessLetter(letter) {
-    console.log(this.letter)
-    
+    console.log(letter)
+    if (this.correctLetters.includes(letter) || (this.incorrectLetters.includes(letter))) {
+      return
+    }
     if (this.word.includes(letter)) {
       console.log("word contains letter");
       this.correctLetters.push(letter);
@@ -38,28 +40,57 @@ class Word {
           correctGuess.push(i);
         }
       }
-
+    let updatedDisplayWord = ''
+    for (let index = 0; index < this.displayWord.length; index++) {
+      const element = this.displayWord[index];
+    if (correctGuess.includes(index)) {
+        updatedDisplayWord = updatedDisplayWord + letter
+      } else {
+        updatedDisplayWord = updatedDisplayWord  + element
+      }
+    }
+    this.displayWord = updatedDisplayWord
+    } else {
+      this.incorrectLetters.push(letter)
+      this.remainingGuesses = this.remainingGuesses -1 
+    }
   }
 
   // implement the updateScreen function:
   updateScreen() {
-
+    let remainingGuesses = document.getElementById('remaining-guesses')
+    remainingGuesses.innerHTML = this.remainingGuesses
+    let incorrectLetters = document.getElementById('incorrect-letters')
+    incorrectLetters.innerHTML = this.incorrectLetters
+    let wordToGuess = document.getElementById('word-to-guess')
+    wordToGuess.innerHTML = this.displayWord
   }
 
   // implement the isGameOver function:
   isGameOver() {
-
+    if (this.remainingGuesses === 0 || this.displayWord === this.word) {
+      return true
+    } else {
+      return false
+    }
   }
 
   // implement the getWinOrLoss function:
   getWinOrLoss() {
-    
+    if (this.word === this.displayWord && this.remainingGuesses > 0) {
+      return 'win'
+    } else if(this.displayWord !== this.word && this.remainingGuesses <= 0) {
+      return 'loss'
+    } else {
+      return null
+    }
   }
 }
 
 function newGame() {
   const randomWord = words[Math.floor(Math.random() * words.length)]
   currentWord = new Word(randomWord)
+  console.log(currentWord)
   currentWord.updateScreen()
 }
 
@@ -72,10 +103,9 @@ document.onkeyup = function(e) {
   currentWord.guessLetter(pressedKey)
   // allow word obj to update screen
   currentWord.updateScreen()
-
   // check if game is over
   const gameOver = currentWord.isGameOver()
-
+console.log(gameOver)
   // if game is over, update wins/losses and start new game
   if (gameOver) {
     const previousWord = document.getElementById('previous-word')
